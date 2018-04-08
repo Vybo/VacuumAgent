2b6ca145c2b3dbe7f4aaaf8e77c1b45e522d2bd9
Dan Vybiral <the.vybo@gmail.com>
2018-04-08 11:39:57 +0000

    Loading of maze?

--- error_messages.py
+++ error_messages.py
@@ -3,12 +3,16 @@ def value_integer_error():
 
 
 def required_parameter_missing(parameter):
-    print("[Loading Error] Required parameter (%s) not supplied.")
+    print("[Loading Error] Required parameter %(parameter)s not supplied." %{"parameter": parameter})
 
 
 def wrong_coordinate(coordinate):
-    print("[Loading Error] Supplied unsupported coordinate (%coordinate)s. Supported format is: a0 [letter][unsigned integer]." %{'coordinate': coordinate})
+    print("[Loading Error] Supplied unsupported coordinate %(coordinate)s. Supported format is: a0 [letter][unsigned integer]." %{"coordinate": coordinate})
 
 
 def coordinate_over_n(coordinate, range):
-    print("[Loading Error] Supplied coordinate (%coordinate)s is outside maze range (%range)i." %{'coordinate': coordinate, "range": range})
+    print("[Loading Error] Supplied coordinate %(coordinate)s is outside maze range %(range)i." %{"coordinate": coordinate, "range": range})
+
+
+def coordinates_over_n(range):
+    print("[Loading Error] Supplied coordinates outside maze range %(range)i." %{"range": range})
--- vacuum.py
+++ vacuum.py
@@ -4,6 +4,7 @@ from robot import Robot
 from room import Room
 from maze import Maze
 import error_messages
+import helper
 
 
 arguments = argsparser.parse_args(sys.argv[1:])
@@ -17,7 +18,7 @@ def load():
 
     if "-n" in arguments.keys():
         try:
-            maze_size = int(arguments["-c"])
+            maze_size = int(arguments["-n"])
         except ValueError:
             error_messages.value_integer_error()
             return False
@@ -26,13 +27,27 @@ def load():
         return False
 
     if "-c" in arguments.keys():
-        return True
+        chess_coordinates = arguments["c"].split(",")
+        chess_x_coordinates = [helper.chess_coordinate_to_x(x) for x in chess_coordinates]
+        chess_y_coordinates = [helper.chess_coordinate_to_y(y) for y in chess_coordinates]
+
+        inaccessible_rooms = []
+        if len(chess_x_coordinates)-1 < maze_size and len(chess_y_coordinates)-1 < maze_size:
+            for i in range(0, len(chess_x_coordinates)):
+                inaccessible_rooms.append(Room(chess_x_coordinates[i], chess_y_coordinates[i], False))
+
+            return Maze(maze_size, inaccessible_rooms)
+        else:
+            error_messages.coordinates_over_n(maze_size)
+            return False
     else:
         error_messages.required_parameter_missing("-c")
         return False
 
 
-if load():
+maze = load()
+
+if maze:
     exit(0)
 else:
     print("Terminated with errors.")
